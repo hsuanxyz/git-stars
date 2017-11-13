f9df6909063fc704fefd8ad64e3d09b4bfc6f15f
Hsuan Lee <HsuanGM@gmail.com>
2017-11-13 23:17:59 +0800

    add(component: dnd-chip): add component style

--- src/app/dnd-chip/dnd-chip.component.ts
+++ src/app/dnd-chip/dnd-chip.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'gs-dnd-chip',
   templateUrl: './dnd-chip.component.html',
   styleUrls: ['./dnd-chip.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class DndChipComponent implements OnInit {
 
