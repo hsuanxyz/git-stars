e2e68d8b3c430214cf375859035df967faab1cc1
Hsuan Lee <HsuanGM@gmail.com>
2017-11-18 16:09:46 +0800

    add(service: github) add getStarredCount method

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,25 +1,62 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as parse from 'parse-link-header';
+import 'rxjs/add/operator/map';
+
+export class PaginationParams {
+  username: string;
+  perPage: number;
+  page: number;
+}
 
 @Injectable()
 export class GithubService {
 
-  constructor(private http: HttpClient) { }
-
-  user(name: string) {
-    this.http.get(`github:users/${name}`)
-    .subscribe(res => console.log(res));
+  constructor(private http: HttpClient) {
   }
 
-  stars(params: { name: string; perPage: number; page: number }) {
-    const { name, perPage, page } = params;
-    this.http.get(`github:users/${name}/starred?per_page=${perPage}&page=${page}`)
-    .subscribe(res => console.log(res));
+
+  /**
+   * @param {string} username
+   * @returns {Observable<Object>}
+   */
+  user(username: string) {
+    return this.http.get(`github:users/${username}`);
   }
 
-  gists(params: { name: string; perPage: number; page: number }) {
-    const { name, perPage, page } = params;
-    this.http.get(`github:users/${name}/gists?per_page=${perPage}&page=${page}`)
-    .subscribe(res => console.log(res));
+  /**
+   * @param {PaginationParams} params
+   * @returns {Observable<Object>}
+   */
+  stars(params: PaginationParams) {
+    const {username, perPage, page} = params;
+    return this.http.get(`github:users/${username}/starred?per_page=${perPage}&page=${page}`);
+  }
+
+  /**
+   * @param {PaginationParams} params
+   * @returns {Observable<Object>}
+   */
+  gists(params: PaginationParams) {
+    const {username, perPage, page} = params;
+    return this.http.get(`github:users/${username}/gists?per_page=${perPage}&page=${page}`);
+  }
+
+  /**
+   * @param {string} username - Github username
+   * @returns {Observable<number>} starred count
+   */
+  getStarredCount(username: string) {
+    return this.http.get(`github:users/${username}/starred?per_page=1&page=1`, {observe: 'response'})
+    .map(res => {
+      if (res.headers.has('link')) {
+        // from https://github.com/egoist/starred-count/blob/master/index.js#L22
+        const parsed = parse(res.headers.get('link'));
+        return (parsed && parsed.last) ? parseInt(parsed.last.page, 10) : 0;
+      } else {
+        throw new Error('not find count');
+      }
+    });
+
   }
 }
