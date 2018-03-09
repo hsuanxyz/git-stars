6183d5768da87aa7db223313d57700c659108814
Hsuan Lee <HsuanGM@gmail.com>
2018-03-09 14:30:07 +0800

    refactor: use sortCompare static-function in the repo-sort pipe

--- src/app/pipes/repo-sort.pipe.ts
+++ src/app/pipes/repo-sort.pipe.ts
@@ -1,27 +1,37 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { GithubRepo } from '../models/github-repo';
+import { ReposFilterSortType } from '../models/repos-filter';
 
 /**
  * 用于库的排序
  */
 @Pipe({name: 'repoSort'})
 export class RepoSortPipe implements PipeTransform {
-  transform(allRepos: GithubRepo[], sort: string): GithubRepo[] {
-    switch (sort) {
+
+  static sortCompare(a: GithubRepo, b: GithubRepo, type: ReposFilterSortType): number {
+    switch (type) {
       case 'star-date':
-        return allRepos.sort((a, b) => a.index - b.index);
+        return a.index - b.index;
 
       case 'most-stars':
-        return allRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+        return b.stargazers_count - a.stargazers_count;
 
       case 'most-fork':
-        return allRepos.sort((a, b) => b.forks_count - a.forks_count);
+        return b.forks_count - a.forks_count;
 
       case 'last-active':
-        return allRepos.sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime());
+        return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime();
 
       default:
-        return allRepos;
+        return 0;
+    }
+  }
+
+  transform(allRepos: GithubRepo[], sort: ReposFilterSortType): GithubRepo[] {
+    if (sort as ReposFilterSortType) {
+      return allRepos.sort((a, b) => RepoSortPipe.sortCompare(a, b, sort));
+    } else {
+      return allRepos;
     }
   }
 }
